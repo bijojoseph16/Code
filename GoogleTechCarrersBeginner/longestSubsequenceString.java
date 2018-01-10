b15d3fb0838f20517729159f3b54613881b6a38e
Bijo Joseph <bjoseph4@ncsu.edu>
2018-01-10 12:48:51 -0500

    Code first approach

--- GoogleTechCarrersBeginner/longestSubsequenceString.java
+++ GoogleTechCarrersBeginner/longestSubsequenceString.java
@@ -13,9 +13,83 @@ The words "able" and "ale" are both subsequences of S, but they are shorter than
 The word "bale" is not a subsequence of S because even though S has all the right letters, they are not in the right order.
 The word "kangaroo" is the longest word in D, but it isn't a subsequence of S.
 */
+import java.util.*;
 
 class longestSubsequenceString {
+    
+    public static void toString(Map<String, List<Integer>> map) {
+      for(Map.Entry<String, List<Integer>> entry : map.entrySet()) {
+        System.out.println(entry.getKey() + ":" + Arrays.toString(entry.getValue().toArray()));
+      }
+    }
+    
+    public static int binarySearch(Integer[] array, int start, int end, int tmp) {
+        while(start <= end) {
+          int mid = start + (end - start)/2;
+          if(array[mid] > tmp) {
+            end = mid - 1;
+          }
+          else {
+            start = mid + 1; 
+          }
+        }
+        return end + 1 == array.length?-1:array[end+1];
+    }
     public static void main(String[] args) {
-        System.out.println("Longest subsequence");
+        List<String> D = new ArrayList<String>();
+        D.add("able");
+        D.add("ale");
+        D.add("apple");
+        D.add("bale");
+        D.add("kangroo");
+
+        //A comparator to sort the strings by length
+        Collections.sort(D, new Comparator<String>() {
+            public int compare(String a, String b) {
+                return Integer.compare(b.length(), a.length());
+            }
+        });
+
+        String S = "abppplee";
+
+        //Preprocess S to account for the position in S each
+        //alphabet occurs
+        Map<String, List<Integer>> map = new HashMap<String, List<Integer>>();
+        for(int i = 0; i < S.length(); i++) {
+            List index = map.get(String.valueOf(S.charAt(i)));
+            if(index == null) {
+              index = new ArrayList<Integer>();
+              index.add(i);
+              map.put(String.valueOf(S.charAt(i)),index);
+            }
+            else {
+              index.add(i);    
+            }
+        }
+        for(String s:D) {
+            int temp = -1;
+            int newTemp = -1;    
+            int count = 0;
+            for(int i = 0; i < s.length(); i++) {
+                count++;
+                if(map.containsKey(String.valueOf(s.charAt(i)))) {
+                  List<Integer> indices = map.get(String.valueOf(s.charAt(i)));
+                  Integer[] indicesArray = new Integer[indices.size()];
+                  indicesArray = indices.toArray(indicesArray);
+                  newTemp = binarySearch(indicesArray, 0, indices.size()-1, temp);
+                  if(newTemp > temp) {
+                      temp = newTemp; 
+                  }
+                }
+                else {
+                    break;
+                }
+            }
+            if(count == s.length()) {
+                System.out.println(s);
+                break;
+            }
+        }
+       
     }
 }
